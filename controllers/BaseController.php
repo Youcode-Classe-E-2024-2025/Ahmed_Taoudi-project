fb05e4a47538a6042ff23dcdcf44bf6894af4174
Tawdi <ahmedtawdi00@gmail.com>
2025-01-02 16:39:55 +0100

    ajoute controller parent  (youtube )

--- controllers/BaseController.php
+++ controllers/BaseController.php
@@ -10,20 +10,29 @@ class BaseController {
     }
 
     protected function checkSession() {
-        session_start();
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
         if (isset($_SESSION['user'])) {
             $this->user = $_SESSION['user'];
         }
     }
 
     protected function requireAuth() {
-        if (!$this->user) {
-            header('Location: /login');
-            exit;
+        if (!$this->isLoggedIn()) {
+            $_SESSION['error'] = 'Please login to continue';
+            $this->redirect('/login');
         }
     }
 
+    protected function isLoggedIn() {
+        return isset($_SESSION['user']) && !empty($_SESSION['user']);
+    }
+
     protected function render($view, $data = []) {
+        $data['user'] = $this->user;
+        $data['isLoggedIn'] = $this->isLoggedIn();
+        
         extract($data);
         require "views/{$view}.php";
     }
@@ -33,4 +42,32 @@ class BaseController {
         exit;
     }
 
+    protected function getRequestMethod() {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+    protected function isPost() {
+        return $this->getRequestMethod() === 'POST';
+    }
+
+    protected function isGet() {
+        return $this->getRequestMethod() === 'GET';
+    }
+
+    protected function setFlashMessage($type, $message) {
+        $_SESSION[$type] = $message;
+    }
+
+    protected function hasFlashMessage($type) {
+        return isset($_SESSION[$type]);
+    }
+
+    protected function getFlashMessage($type) {
+        if ($this->hasFlashMessage($type)) {
+            $message = $_SESSION[$type];
+            unset($_SESSION[$type]);
+            return $message;
+        }
+        return null;
+    }
 }
