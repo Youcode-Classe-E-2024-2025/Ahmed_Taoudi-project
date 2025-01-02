34d7bca05d02136602b19e85b2be548a4c381e74
Tawdi <ahmedtawdi00@gmail.com>
2025-01-02 11:40:54 +0100

    update index.php require router et les route V.1

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-<?php 
+<?php
+session_start();
+require "assets/helper/fonctions.php";
+require_once "models/Router.php";
+require_once "models/Database.php";
+
+// CSRF
+if (empty($_SESSION['csrf_token'])) { 
+
+    $_SESSION['csrf_token'] = bin2hex(random_bytes(32)); // Generates a 64-character hex token
+}
+
+$db = new Database();
+// 
+if(isset($_POST['createdb'])){ 
+
+    if(isset($_POST['checkbox']) && $_POST['checkbox'] == 'on' ){
+        
+        $db->createDatabase(DBNAME,true);
+    }else{
+        
+        $db->createDatabase(DBNAME,false);
+    }
+    
+}
+
+$router = new Router($db);
+
+require "routes.php";
+
+$router->resolve($_SERVER['REQUEST_URI']);
